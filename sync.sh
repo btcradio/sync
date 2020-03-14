566c93bad907c87211ed41a06635ab31d97f0437
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-03-14 11:33:06 +0000

    .

--- sync.sh
+++ sync.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+git clone https://github.com/btcradio/upodder.git
 
 upodder
 mv ~/Downloads/podcasts/*.mp3 /var/lib/docker/volumes/azuracast_station_data/_data/btcradio.net/media/
