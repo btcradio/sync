624a098c2500a4ca00620501aeb96883778d7cc1
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-03-14 22:21:02 +0000

    more config for subscriptions  Changes to be committed: 	new file:   subscriptions 	modified:   sync.sh

--- sync.sh
+++ sync.sh
@@ -2,5 +2,10 @@
 
 git clone https://github.com/btcradio/upodder.git
 
+cd upodder
+
+
+
+
 upodder
 mv ~/Downloads/podcasts/*.mp3 /var/lib/docker/volumes/azuracast_station_data/_data/btcradio.net/media/
