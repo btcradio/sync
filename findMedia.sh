bedd98328504c7405e8bfe3f870836ee0f3bf214
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-03-16 19:31:30 +0000

    modified:   findMedia.sh

--- findMedia.sh
+++ findMedia.sh
@@ -4,26 +4,25 @@
 
 echo
 echo "Docker Volumes:"
-
 find / -iname "volumes" -print
 
 echo
-
 echo "Station Data:"
 echo "ls /var/lib/docker/volumes/azuracast_station_data/_data/"
 ls /var/lib/docker/volumes/azuracast_station_data/_data/
 
 echo
-
 echo "btcRadio.net Directory:"
 find /var/lib/docker/volumes/ -iname "btcradio.net" -print
 
 echo
 echo "Relavent html files:"
-
 find /var/lib/docker/volumes/ -iname "*.html" -print
 
 echo
-echo "Location of synced podcast episodes:"
+echo "Location of where upodder default downloads episodes:"
+find $HOME -type d -name podcasts
 
+echo
+echo "Location of synced podcast episodes:"
 find /var/lib/docker/volumes/ -iname "*.mp3" -print
