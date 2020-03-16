32e18aba20f94c70bb7d4e63bb9aa1eb7b3569c4
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-03-16 18:25:04 +0000

    .

--- sync.sh
+++ sync.sh
@@ -1,16 +1,24 @@
 #!/usr/bin/env bash
 
+apt-get install upodder
+git clone https://github.com/btcradio/upodder.git
+
 if [ -f /root/.upodder/subscriptions ]; then
+
    mv /root/.upodder/subscriptions /root/.upodder/subscriptions.$(date +%s | cut -b1-13)
+   cp subscriptions /root/.upodder/subscriptions
+
 else
-mkdir -p  /root/.upodder
+
+    mkdir -p  /root/.upodder
+    cp subscriptions /root/.upodder/subscriptions
+
 fi
 
-cp subscriptions /root/.upodder/subscriptions
-
-git clone https://github.com/btcradio/upodder.git
+#REF: /var/lib/docker/volumes/azuracast_station_data/_data/btcradio.net/media/
+MEDIA_DIRECTORY = '/var/lib/docker/volumes/azuracast_station_data/_data/btcradio.net/media/'
 
 upodder
 
-mv ~/Downloads/podcasts/*.mp3 /var/lib/docker/volumes/azuracast_station_data/_data/btcradio.net/media/
+mv ~/Downloads/podcasts/*.mp3 $MEDIA_DIRECTORY
 
