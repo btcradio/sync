63e1759673f34a14c2026a2c4a3083fef0cf2aed
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-03-16 21:18:54 +0000

    build or DL libbitcoin-explorer

--- installLibbitcoin-explorer.sh
+++ installLibbitcoin-explorer.sh
@@ -4,17 +4,24 @@
 
 sudo apt update
 
+sudo apt install wget
+
 sudo apt install espeak #for saying block height in broadcast
-sudo apt-get install g++-4.8
-sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
-sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-4.8 50
-sudo apt-get install build-essential autoconf automake libtool pkg-config git
 
-./install.sh --prefix=/home/me/myprefix --build-boost --build-zmq --disable-shared
+#Build libbitcoin-explorer from source # Alot of work
 
+#sudo apt-get install g++-4.8
+#sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-4.8 50
+#sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-4.8 50
+#sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-4.8 50
+#sudo apt-get install build-essential autoconf automake libtool pkg-config git
+##
+#wget https://raw.githubusercontent.com/libbitcoin/libbitcoin-explorer/version3/install.sh
+#chmod +x install.sh
+#./install.sh --prefix=/root/bx --build-boost --build-zmq --disable-shared
 
-
+wget https://github.com/libbitcoin/libbitcoin-explorer/releases/download/v3.2.0/bx-linux-x64-qrcode
+ln -s bx-linux-x64-qrcode bx
 
 
 #sudo systemctl enable cron
