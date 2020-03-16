1c588e7ea7852e9b281b466768e7130d74908f33
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-03-16 18:34:52 +0000

    .

--- sync.sh
+++ sync.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
-apt-get install upodder
-git clone https://github.com/btcradio/upodder.git
+#apt-get install upodder
+#git clone https://github.com/btcradio/upodder.git
 
 if [ -f /root/.upodder/subscriptions ]; then
 
-   mv /root/.upodder/subscriptions /root/.upodder/subscriptions.$(date +%s | cut -b1-13)
-   cp subscriptions /root/.upodder/subscriptions
+    sleep 5
+
+    mv /root/.upodder/subscriptions /root/.upodder/subscriptions.$(date +%s | cut -b1-13)
+    cp subscriptions /root/.upodder/subscriptions
 
 else
+
+    sleep 5
 
     mkdir -p  /root/.upodder
     cp subscriptions /root/.upodder/subscriptions
@@ -16,9 +20,14 @@ else
 fi
 
 #REF: /var/lib/docker/volumes/azuracast_station_data/_data/btcradio.net/media/
-MEDIA_DIRECTORY = '/var/lib/docker/volumes/azuracast_station_data/_data/btcradio.net/media/'
+set MEDIA_DIRECTORY = /var/lib/docker/volumes/azuracast_station_data/_data/btcradio.net/media/
+
+    sleep 5
 
 upodder
 
-mv ~/Downloads/podcasts/*.mp3 $MEDIA_DIRECTORY
+    sleep 5
+
+#mv ~/Downloads/podcasts/*.mp3 $MEDIA_DIRECTORY
+mv ~/Downloads/podcasts/*.mp3 /var/lib/docker/volumes/azuracast_station_data/_data/btcradio.net/media/
 
