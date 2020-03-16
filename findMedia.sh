30a100790bb521ecccfcb20a07b7c558f4c6e495
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-03-16 19:24:05 +0000

    .

--- findMedia.sh
+++ findMedia.sh
@@ -3,21 +3,27 @@
 #REF: https://www.cyberciti.biz/tips/howto-linux-unix-find-move-all-mp3-file.html
 
 echo
-echo
+echo "Docker Volumes:"
 
 find / -iname "volumes" -print
 
 echo
+
+echo "Station Data:"
+echo "ls /var/lib/docker/volumes/azuracast_station_data/_data/"
+ls /var/lib/docker/volumes/azuracast_station_data/_data/
+
 echo
 
+echo "btcRadio.net Directory:"
 find /var/lib/docker/volumes/ -iname "btcradio.net" -print
 
 echo
-echo
+echo "Relavent html files:"
 
 find /var/lib/docker/volumes/ -iname "*.html" -print
 
 echo
-echo
+echo "Location of synced podcast episodes:"
 
 find /var/lib/docker/volumes/ -iname "*.mp3" -print
