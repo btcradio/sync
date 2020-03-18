94113d3667653366fa515e2ed362ba315498a72e
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-03-18 01:55:24 +0000

    modified:   findConfigs.sh

--- logs/findConfigs.sh
+++ logs/findConfigs.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-#REF: https://www.cyberciti.biz/tips/howto-linux-unix-find-move-all-mp3-file.html
-
 echo
 echo "Docker Volumes:"
 find / -iname "volumes" -print
@@ -13,25 +11,14 @@ ls /var/lib/docker/volumes/azuracast_station_data/_data/
 
 echo
 echo "btcRadio.net logs and config files.:"
-find /var/lib/docker/volumes/ -iname "*.log" -print
 find /var/lib/docker/volumes/ -iname "*.liq" -print
 find /var/lib/docker/volumes/ -iname "*.xml" -print
 echo
 
-
 echo Files will look simular to this within the containers.
-echo
-echo Liquidsoap Log
-echo /var/azuracast/stations/btcradio.net/config/liquidsoap.log
 echo
 echo Liquidsoap Configuration
 echo /var/azuracast/stations/btcradio.net/config/liquidsoap.liq
-echo
-echo Icecast Access Log
-echo /var/azuracast/stations/btcradio.net/config/icecast_access.log
-echo
-echo Icecast Error Log
-echo /var/azuracast/stations/btcradio.net/config/icecast.log
 echo
 echo Icecast Configuration
 echo /var/azuracast/stations/btcradio.net/config/icecast.xml
