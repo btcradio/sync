6988e9f11425999f29cb3096abc71f3278a1cca3
randymcmillan <randy.lee.mcmillan@gmail.com>
2020-03-18 02:29:46 +0000

    modified:   configGithub.sh

--- configGithub.sh
+++ configGithub.sh
@@ -7,16 +7,16 @@ configGithub() {
     if [[ $REPLY =~ ^[Yy]$ ]]; then
     #REF:https://help.github.com/en/github/authenticating-to-github/checking-for-existing-gpg-keys
     gpg --list-secret-keys --keyid-format LONG
-    echo randymcmillan | read -p 'ENTER your Github.com username: ' GITHUB_USER_NAME
+    read -p 'ENTER your Github.com username: ' GITHUB_USER_NAME
     #read -sp 'Password: ' GITHUB_USER_PASSWORD
     git config --global user.name $GITHUB_USER_NAME
     echo Thankyou $GITHUB_USER_NAME
-    echo randy.lee.mcmillan@gmail.com | read -p 'ENTER your Github.com user email: ' GITHUB_USER_EMAIL
+    read -p 'ENTER your Github.com user email: ' GITHUB_USER_EMAIL
     git config --global user.email $GITHUB_USER_EMAIL
     echo Thankyou $GITHUB_USER_NAME for your email.
-    echo 97966C06BB06757B | read -p 'ENTER your public gpg signing key id: ' PUBLIC_GPG_SIGNING_KEY_ID
-    git config --global user.signingkey $PUBLIC_GPG_SIGNING_KEY_ID
-    echo Thankyou $GITHUB_USER_NAME for public gpg signing key id.
+    #read -p 'ENTER your public gpg signing key id: ' PUBLIC_GPG_SIGNING_KEY_ID
+    #git config --global user.signingkey $PUBLIC_GPG_SIGNING_KEY_ID
+    #echo Thankyou $GITHUB_USER_NAME for public gpg signing key id.
     fi;
 
     #REF: https://gist.github.com/grenade/6318301
@@ -27,18 +27,29 @@ configGithub() {
     chmod 644 ~/.ssh/known_hosts
     chmod 644 ~/.ssh/config
 
+    echo Creating the ssh keys
     DATE=$(date +%s)
     ssh-keygen -t rsa -b 4096 -N '' -C $GITHUB_USER_EMAIL -f ~/.ssh/$DATE.github_rsa
-
+echo
     eval "$(ssh-agent -s)"
-    ssh-add ~/.ssh/$DATE.id_rsa
+echo
     ssh-add ~/.ssh/$DATE.github_rsa
-    ssh-add ~/.ssh/$DATE.mozilla_rsa
-
+echo
     sudo chmod 600 ~/.ssh/$DATE.github_rsa
+echo
     sudo chmod 644 ~/.ssh/$DATE.github_rsa.pub
+echo
+    cat  ~/.ssh/$DATE.github_rsa.pub
+echo
+    echo add this to you github account...
+echo
 
 }
+sudo
+mkdir -p ~/.ssh
+mkdir -p ~/.ssh/authorized_keys
+mkdir -p ~/.ssh/known_hosts
+mkdir -p ~/.ssh/config
 configGithub
 
 
